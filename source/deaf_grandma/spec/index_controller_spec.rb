fca726c3af123c51a24b0778f93847f4c0372da2
aespaldi <aespaldi@gmail.com>
2014-03-31 11:34:27 -0700

    fix sample tests

--- source/deaf_grandma/spec/index_controller_spec.rb
+++ source/deaf_grandma/spec/index_controller_spec.rb
@@ -11,13 +11,14 @@ describe "IndexController (this is a skeleton controller test!)" do
     # Can you think of more meaningful GET tests to write?
   end
   describe 'post' do
-    # Here is a sample POST test for you to study
+    # Here is a sample POST test for you to study - feel free to modify for your code.
     it 'should return a response for uppercase' do
       post '/grandma', {user_input: "HI"}
-      expect(last_response).body.include? ("HI")
+      expect(last_response.body).to include("HI")
     end
 
     it 'should return a response for lowercase' do
+     pending
       # Write your POST test here
     end
 
