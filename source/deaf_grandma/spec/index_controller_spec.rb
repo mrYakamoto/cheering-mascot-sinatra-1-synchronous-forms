a6388dc756b4db26771a28527552129fc6e69511
raorao <savinirs@gmail.com>
2014-04-19 14:02:48 -0700

    makes tests functional
    - get / test now tests render instead of status, since
    status tests are not particularly meaningful
    
    - post / test skeleton now tests location to handle redirect
    - post route params now matches form content

--- source/deaf_grandma/spec/index_controller_spec.rb
+++ source/deaf_grandma/spec/index_controller_spec.rb
@@ -4,17 +4,17 @@ describe "IndexController (this is a skeleton controller test!)" do
 
   describe 'get grandma' do
     # Here is a sample GET test for you to study
-    it 'should return a response' do
-      get "/"
-      expect(last_response).to be_ok
+    it "should render grandma's response" do
+      get "/", { grandma: 'HI' }
+      expect(last_response.body).to include("HI")
     end
     # Can you think of more meaningful GET tests to write?
   end
   describe 'post' do
-    # Here is a sample POST test for you to study - feel free to modify for your code.
-    it 'should return a response for uppercase' do
-      post '/grandma', {grandma: "HI"}
-      expect(last_response.body).to include("HI")
+    # Here is a sample POST test for you to study - modify for your code.
+    it "should return a response for uppercase" do
+      post '/grandma', { user_input: "HI GRANDMA" }
+      expect(last_response.location).to include("NOT SINCE 1979")
     end
 
     it 'should return a response for lowercase' do
